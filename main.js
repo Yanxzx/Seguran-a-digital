3e84d9a253fd23a9c5d183becd1d30a1f22c5b9e
Yanxzx <matheus.freitas.silva07@escola.pr.gov.br>
2026-06-22 11:43:23 +0000

    ....

--- main.js
+++ main.js
@@ -64,13 +64,13 @@ function geraSenha() {
 function classificaSenha(tamanhoAlfabeto){
     let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
     console.log(entropia);
-    forcaSenha.classList.remove('fraca','media','forte');
+    forcaSenha.classList.remove('hackeado','media','esqueceu = perdeu');
     if (entropia > 57){
-        forcaSenha.classList.add('forte');
+        forcaSenha.classList.add('esqueceu = perdeu');
     } else if (entropia > 35 && entropia < 57 ) {
         forcaSenha.classList.add('media');
     } else if (entropia <= 35){
-        forcaSenha.classList.add('fraca');
+        forcaSenha.classList.add('hackeado');
     }
     const valorEntropia = document.querySelector('.entropia');
     valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
